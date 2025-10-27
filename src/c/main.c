3a2ef72668516eeeaeb27c282ca175b660483919
jedrasiak <lukasz@jedrasiak.eu>
2025-10-27 23:51:11 +0100

    feat(cli): add nn command to create new notes
    - implement nn function to accept note title as argument
    - update command parsing to handle nn command with title parameter
    - reorder help/version checks to prioritize version flag

--- src/c/main.c
+++ src/c/main.c
@@ -5,6 +5,7 @@ char *VERSION = "0.1.0";
 
 int help();
 int version();
+int nn(char *title);
 
 int main(int argc, char *argv[]) {
     //printf("argc: %d\n", argc);
@@ -15,17 +16,26 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc == 2) {
-        if ((strcmp(argv[1], "--help") == 0) || (strcmp(argv[1], "-h") == 0)) {
+        if ((strcmp(argv[1], "--version") == 0) || (strcmp(argv[1], "-v") == 0)) {
+            version();
+        } else {
             help();
         }
     }
 
-    if (argc == 2) {
-        if ((strcmp(argv[1], "--version") == 0) || (strcmp(argv[1], "-v") == 0)) {
-            version();
+    if (argc == 3) {
+        if (strcmp(argv[1], "nn") == 0) {
+            nn(argv[2]);
+        } else {
+            help();
         }
     }
 
+    return 0;
+}
+
+int nn(char *title) {
+    printf("Creating a new note with title: %s\n", title);
     return 0;
 }
 
