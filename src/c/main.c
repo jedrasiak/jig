aa41c4cbaa6cbdda4b04ae929a49a92c893b7bda
jedrasiak <lukasz@jedrasiak.eu>
2025-10-27 23:22:25 +0100

    feat(cli): add version flag and display
    Implement version information display:
    - Add VERSION constant set to 0.1.0
    - Add version() function to print version
    - Support --version/-v flags

--- src/c/main.c
+++ src/c/main.c
@@ -1,7 +1,10 @@
 # include <stdio.h>
 # include <string.h>
 
+char *VERSION = "0.1.0";
+
 int help();
+int version();
 
 int main(int argc, char *argv[]) {
     //printf("argc: %d\n", argc);
@@ -16,6 +19,13 @@ int main(int argc, char *argv[]) {
             help();
         }
     }
+
+    if (argc == 2) {
+        if ((strcmp(argv[1], "--version") == 0) || (strcmp(argv[1], "-v") == 0)) {
+            version();
+        }
+    }
+
     return 0;
 }
 
@@ -30,5 +40,10 @@ int help() {
     printf("Options:\n");
     printf("  -h, --help       Show this help message and exit\n");
     printf("  -v, --version    Show version information\n");
+    return 0;
+}
+
+int version() {
+    printf("%s\n", VERSION);
     return 0;
 }
