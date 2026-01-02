1a510a08e4e2ebb568e81124e0f76790907ce0d4
jedrasiak <lukasz@jedrasiak.eu>
2026-01-02 23:05:37 +0100

    feat(node): create node module with core data structures
    Introduce Node and NodeList structures for managing tree nodes. Update build configuration to enable cross-module includes with -I.. flag.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Compiler and flags
 CC = gcc
-CFLAGS = -Wall -Wextra -I./src
+CFLAGS = -Wall -Wextra -Werror -I./src
 LDFLAGS =
 
 # Directories
---
+++ src/node/node.h
@@ -0,0 +1,17 @@
+#ifndef NODE_H
+#define NODE_H
+
+typedef struct {
+    char id[37];
+    char *path;
+    char *link;
+    char *title;
+} Node;
+
+typedef struct {
+    Node *items;
+    int count;
+    int capacity;
+} NodeList;
+
+#endif /* NODE_H */
